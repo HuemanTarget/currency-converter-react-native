7d6949c73fcd57017fa0589d0ea23f4c5890ba2d
PompousPro <pompouspro@PompousPros-MacBook-Pro.local>
2020-06-26 15:41:09 -0700

    add currency buttons and functionality

--- App.js
+++ App.js
@@ -6,48 +6,112 @@ import {
   SafeAreaView,
   TextInput,
   TouchableOpacity,
+  Alert,
+  Keyboard,
+  TouchableWithoutFeedback,
 } from "react-native";
 
 const currencyPerDollar = {
   EURO: 0.89,
   POUND: 0.81,
   RUBLE: 69.13,
-  BITCOIN: 0.00011,
   YEN: 107.2,
   DINAR: 1190.0,
-  RUPEE: 75.56,
   SHEKEL: 3.44,
   AUSDOLLAR: 1.45,
   CANDOLLAR: 1.36,
+  BITCOIN: 0.00011,
 };
 
 export default function App() {
   const [inputValue, setInputValue] = useState(20);
   const [resultValue, setResultValue] = useState(0.0);
 
+  buttonPressed = (currency) => {
+    if (inputValue === "") {
+      Alert.alert("Please enter a value");
+    }
+    let result = parseFloat(inputValue) * currencyPerDollar[currency];
+
+    setResultValue(result.toFixed(2));
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.screenview}>
-        <View style={styles.resultcontainer}>
-          <Text style={styles.results}>{resultValue}</Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.screenview}>
+          <View style={styles.resultcontainer}>
+            <Text style={styles.results}>{resultValue}</Text>
+          </View>
+          <View style={styles.inputcontainer}>
+            <TextInput
+              style={styles.input}
+              selectionColor="white"
+              keyboardType="numeric"
+              placeholder="Enter US $ Value"
+              value={inputValue}
+              onChangeText={(inputValue) => setInputValue(inputValue)}
+            />
+          </View>
+          <View style={styles.converterbuttoncontainer}>
+            <TouchableOpacity
+              onPress={() => buttonPressed("EURO")}
+              style={styles.converterbutton}
+            >
+              <Text style={styles.converterbuttontext}>Euro</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => buttonPressed("POUND")}
+              style={styles.converterbutton}
+            >
+              <Text style={styles.converterbuttontext}>Pound</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => buttonPressed("RUBLE")}
+              style={styles.converterbutton}
+            >
+              <Text style={styles.converterbuttontext}>Ruble</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => buttonPressed("YEN")}
+              style={styles.converterbutton}
+            >
+              <Text style={styles.converterbuttontext}>Yen</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => buttonPressed("DINAR")}
+              style={styles.converterbutton}
+            >
+              <Text style={styles.converterbuttontext}>Dinar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => buttonPressed("SHEKEL")}
+              style={styles.converterbutton}
+            >
+              <Text style={styles.converterbuttontext}>Shekel</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => buttonPressed("AUSDOLLAR")}
+              style={styles.converterbutton}
+            >
+              <Text style={styles.converterbuttontext}>AUS Dollar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => buttonPressed("CANDOLLAR")}
+              style={styles.converterbutton}
+            >
+              <Text style={styles.converterbuttontext}>CAN Dollar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => buttonPressed("BITCOIN")}
+              style={styles.converterbutton}
+            >
+              <Text style={styles.converterbuttontext}>Bitcoin</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={styles.inputcontainer}>
-          <TextInput
-            style={styles.input}
-            selectionColor="white"
-            keyboardType="numeric"
-            placeholder="Enter Value"
-            value={inputValue}
-            onChangeText={(inputValue) => setInputValue(inputValue)}
-          />
-        </View>
-        <View style={styles.converterbuttoncontainer}>
-          <TouchableOpacity style={styles.converterbutton}>
-            <Text style={styles.converterbuttontext}>$</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -61,7 +125,7 @@ const styles = StyleSheet.create({
   },
   resultcontainer: {
     height: 70,
-    marginTop: 20,
+    marginTop: 30,
     justifyContent: "center",
     alignItems: "center",
     borderColor: "#c1c1c1",
@@ -85,6 +149,7 @@ const styles = StyleSheet.create({
   input: {
     color: "white",
     fontSize: 30,
+    fontWeight: "bold",
   },
   converterbuttoncontainer: {
     flexDirection: "row",
